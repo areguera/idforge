ce7768a5f4f4a59e55fa90c2187a2475abe76965
Alain Reguera Delgado <alain.reguera@gmail.com>
2015-01-04 20:40:19 -0500

    Update variable names in script_printModulesList

---
+++ Library/Modules/Usage/Modules/Script/script_printModulesList.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+######################################################################
+#
+#   idforge - Manage identity manuals in community.
+#   Copyright © 2015 CentOS Artwork SIG
+#
+#   idforge is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   idforge is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with idforge; if not, write to the Free Software Foundation,
+#   Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#   Alain Reguera Delgado <alain.reguera@gmail.com>
+#   39 Street No. 4426 Cienfuegos, Cuba.
+#
+######################################################################
+
+function script_printModulesList {
+
+    local MODULE_NAME=''
+    local MODULE_DESCRIPTION=''
+
+    for MODULE_NAME in ${MODULE_NAMES};do
+        MODULE_DESCRIPTION=$(idforge_setModuleEnvironment -m ${MODULE_NAME} -t 'parent' -g '--description')
+        idforge_printMessage "   ${MODULE_NAME}: ${MODULE_DESCRIPTION}" --as-stdout-line=8
+    done
+
+}
