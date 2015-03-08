bb34508b0724c377d13d7028559d14b5df79fa2f
Alain Reguera Delgado <alain.reguera@gmail.com>
2015-03-08 16:30:32 -0400

    Update scripts' top comment to be more specific
    - Previously, the top comment said "This program is free software;
      ...". This update changes the shell scripts' top comment to be more
      specific about what "this program" is. So, it now verses "idFORGE
      Framework is free software; ...". This last specification is used in
      the copyright section of documentation manuals, as well.

--- Library/Modules/Hello/hello.sh
+++ Library/Modules/Hello/hello.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 ######################################################################
 #
-#   idforge-modules-hello - Print a greeting and exit successfully.
+#   idFORGE Framework - Manage identity manuals in community
 #   Copyright © 2015 CentOS Artwork SIG
 #
-#   idforge-modules-hello is free software; you can redistribute it
-#   and/or modify it under the terms of the GNU General Public License
-#   as published by the Free Software Foundation; either version 2 of
-#   the License, or (at your option) any later version.
+#   idFORGE Framework is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2 of the
+#   License, or (at your option) any later version.
 #
-#   idforge-modules-hello is distributed in the hope that it will be
+#   idFORGE Framework is distributed in the hope that it will be
 #   useful, but WITHOUT ANY WARRANTY; without even the implied
 #   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #   See the GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with idforge-modules-hello; if not, write to the Free
-#   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#   along with idFORGE Framework; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 #   Alain Reguera Delgado <alain.reguera@gmail.com>
 #   39 Street No. 4426 Cienfuegos, Cuba.
