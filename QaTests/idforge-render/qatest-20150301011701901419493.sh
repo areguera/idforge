c8f78124f112f105131ddfbc4da44cf155dca2cf
Alain Reguera Delgado <alain.reguera@gmail.com>
2015-03-10 22:47:22 -0400

    Update qatest-20150301011701901419493
    - to test several common options in idforge command-line.  For
      example, passing both --debug and --filter options together in a
      single call to idforge command.

--- QaTests/idforge-render/qatest-20150301011701901419493.sh
+++ QaTests/idforge-render/qatest-20150301011701901419493.sh
@@ -18,4 +18,7 @@ QATEST_FLAG_COMMAND="./idforge.sh"
 # Enter integrity checks target arguments:
 #=====================================================================
 command_line "--filter='section-id-1' render ${QATEST_UNIT_DIR}/${QATEST_UNIT_NAME}.conf"
-command_line "--filter='manpage.1.gz' render ${QATEST_UNIT_DIR}/${QATEST_UNIT_NAME}.conf"
+
+# Test the action of passing more than one option to idforge.sh
+# command-line.
+command_line "--debug --filter='manpage.1.gz' render ${QATEST_UNIT_DIR}/${QATEST_UNIT_NAME}.conf"
